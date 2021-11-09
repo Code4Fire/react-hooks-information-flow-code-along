63865676d43ab4d860879b0cb6c09781ea99e219
Tiffany_Tam <tamtiffany89@gmail.com>
2021-11-08 22:26:26 -0500

    Completed

--- src/Child.js
+++ src/Child.js
@@ -1,8 +1,26 @@
 import React from "react";
 import { getRandomColor } from "./randomColorGenerator.js";
 
-function Child({ onChangeColor }) {
-  return <div className="child" style={{ backgroundColor: "#FFF" }} />;
+function Child({ onChangeColor, color }) {
+//<!-- additional function created for child event listener -- Q: How come "handleClick" does not need to be reference in the Parent component?-->
+  function handleClick() {
+    const newColor = getRandomColor();
+    onChangeColor(newColor);
+  }
+  // return (
+  //   <div 
+  //   onClick = {handleClick}
+  //   className = "child"
+  //   style ={{background:color}}
+  //   />
+  // );
+  return (
+  <div  
+    className="child" 
+    style={{ backgroundColor: color }} 
+    onClick= {handleClick, onChangeColor}
+  />
+  );
 }
 
 export default Child;
--- src/Parent.js
+++ src/Parent.js
@@ -4,12 +4,22 @@ import Child from "./Child";
 
 function Parent() {
   const randomColor = getRandomColor();
+// <!-- this line tracks the color change state for the Parent component -->
   const [color, setColor] = useState(randomColor);
+// <!-- setting up the variable to keep track of the color change in the Child component --->
+  const [childrenColor, setChildrenColor] = useState("#FFF");
 
+// <!--this function is to initiate the random color event/function -->
+function handleChangeColor (newChildColor) {
+  // const newRandomColor= getRandomColor();
+  setColor(getRandomColor());
+  setChildrenColor(newChildColor);
+}
+// <!-- return element stores the event function name -->
   return (
     <div className="parent" style={{ backgroundColor: color }}>
-      <Child />
-      <Child />
+      <Child color= {childrenColor} onChangeColor = {handleChangeColor}/>
+      <Child color= {childrenColor} onChangeColor = {handleChangeColor}/>
     </div>
   );
 }
